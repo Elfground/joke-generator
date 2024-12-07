df9a33965080cf234e3685464983efd5a8ada4d5
Elfground <elfground@gmail.com>
2024-12-07 20:21:45 +0100

    Created the core files

---
+++ src/index.js
@@ -0,0 +1,26 @@
+function fetchJokes(response) {
+    //console.log(response.data.answer);
+    let jokeText = document.querySelector("#joke");
+    jokeText.classList.add("joke-delivery");
+    //jokeText.innerHTML = response.data.answer;
+    new Typewriter(jokeText, {
+      strings: response.data.answer,
+      autoStart: true,
+      cursor: "",
+      delay: 50,
+    });
+  }
+  
+  function clicked() {
+    let apiKey = "2f78437a500ef24fc3e9894233eftb0o";
+    let context =
+      "You are a dad joke expert with a preference for christmas jokes and puns";
+    let prompt = "Tell me a good christmas joke";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+    console.log("Processing...");
+    axios.get(apiUrl).then(fetchJokes);
+  }
+  
+  let jokeButtonElement = document.querySelector("#joke-button");
+  jokeButtonElement.addEventListener("click", clicked);
+  
